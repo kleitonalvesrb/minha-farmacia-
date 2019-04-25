7c988ba12ef67edc7394c87c654b57488f7d7b3e
kleitonbatista <kleiton.a.batista@gmail.com>
2019-04-24 23:20:13 -0300

    inicial

---
+++ app/src/main/java/kleitonbatista/com/br/minhafarmcia/Constantes.java
@@ -0,0 +1,6 @@
+package kleitonbatista.com.br.minhafarmcia;
+
+public class Constantes {
+    public static String VALIDACAO_CAMPO = "Esse campo deve ser informado!";
+    public static final String  MSG = "oi";
+}
---
+++ app/src/main/java/kleitonbatista/com/br/minhafarmcia/Login.java
@@ -0,0 +1,64 @@
+package kleitonbatista.com.br.minhafarmcia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    private EditText login;
+    private EditText senha;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setLogin((EditText) findViewById(R.id.etLogin));
+        setSenha((EditText) findViewById(R.id.etSenha));
+    }
+    public void logar(View v){
+        String login = getLogin().getText().toString().trim();
+        String senha = getSenha().getText().toString().trim();
+
+        if (validaCampo(getLogin(),Constantes.VALIDACAO_CAMPO,1) &&
+                validaCampo(getSenha(),Constantes.VALIDACAO_CAMPO,1)){
+            Toast.makeText(this,"Validado",Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+    public EditText getLogin() {
+        return login;
+    }
+
+    public void setLogin(EditText login) {
+        this.login = login;
+    }
+
+    public EditText getSenha() {
+        return senha;
+    }
+
+    public void setSenha(EditText senha) {
+        this.senha = senha;
+    }
+
+    /**
+     *
+     * @param campo
+     * @param msg
+     * @param tamMin
+     * @return
+     */
+    public boolean validaCampo(EditText campo, String msg, int tamMin){
+        boolean retorno = true;
+        if (campo.getText().toString().trim().length() < tamMin){
+            campo.setError(msg);
+            campo.requestFocus();
+            retorno = false;
+        }
+        return retorno;
+    }
+}
